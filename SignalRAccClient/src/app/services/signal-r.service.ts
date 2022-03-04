c98791da4c42b18682faea3262c190c21ac56083
Domantas <domskuja.colab@gmail.com>
2022-03-04 13:24:19 +0200

    implemented graph data refresh

--- SignalRAccClient/src/app/services/signal-r.service.ts
+++ SignalRAccClient/src/app/services/signal-r.service.ts
@@ -13,6 +13,8 @@ export class SignalRService {
 
   public graph = {
     data: [
+        { x: [0], y: [0], type: '', mode: '', name: ''},
+        { x: [0], y: [0], type: '', mode: '', name: ''},
         { x: [0], y: [0], type: '', mode: '', name: ''}
     ]
   };
@@ -33,14 +35,29 @@ export class SignalRService {
       //this.data = data;
       //this.results[0].x.push(2);
       //this.results[0].y.push(2);
+      this.graph.data.push(data);
+      
       this.graph.data[0].x.push(data[0].x[0]);    
       this.graph.data[0].y.push(data[0].y[0]);
       this.graph.data[0].name = data[0].name;
       this.graph.data[0].type = data[0].type;
       this.graph.data[0].mode = 'lines+points';
+
+      this.graph.data[1].x.push(data[1].x[0]);    
+      this.graph.data[1].y.push(data[1].y[0]);
+      this.graph.data[1].name = data[1].name;
+      this.graph.data[1].type = data[1].type;
+      this.graph.data[1].mode = 'lines+points';
+
+      this.graph.data[2].x.push(data[2].x[0]);    
+      this.graph.data[2].y.push(data[2].y[0]);
+      this.graph.data[2].name = data[2].name;
+      this.graph.data[2].type = data[2].type;
+      this.graph.data[2].mode = 'lines+points';
+
       //this.graph.data[0].x.push(this.data[0].x[0]);
       //this.graph.data[0].y.push(this.data[0].x[0]);
-      this.graph.data = data;
+      
       //this.data = this.graph.data;
       //this.data.pipe(data);
     });
